eb9bf36bd1599bc72f020a5e567e3d0c90735940
recepergan <68998200+recepergan@users.noreply.github.com>
2022-10-15 15:42:52 +0300

    signup-page-init

---
+++ frontend/src/UserSignupPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class UserSignupPage extends React.Component {
+
+    render() {
+        return (
+            <h1>Sign Up</h1>
+        );
+    }
+}
+
+export default UserSignupPage;
