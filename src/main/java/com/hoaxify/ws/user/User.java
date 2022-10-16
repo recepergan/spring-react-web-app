c507fc5846618fb52c35dfc80a4cef1872da8a06
recepergan <68998200+recepergan@users.noreply.github.com>
2022-10-16 11:59:24 +0300

    saving-user-to-database

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -2,13 +2,28 @@ package com.hoaxify.ws.user;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity// @Table ile databasedeki tablo adını istediğimiz şekilde belirleyebiliriz
+@Table(name="users")
 public class User {
-	
+	@Id
+	@GeneratedValue
+	private long id;
+
 	private String username;
 	
 	private String displayName;
 	
 	private String password;
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 }
