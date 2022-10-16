95c8f5a2d097d55ac67ef7916e9601f70e5de693
recepergan <68998200+recepergan@users.noreply.github.com>
2022-10-16 12:37:59 +0300

    refactor-user-controller

---
+++ src/main/java/com/hoaxify/ws/shared/GenericResponse.java
@@ -0,0 +1,14 @@
+package com.hoaxify.ws.shared;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+public class GenericResponse {
+
+    private String message;
+
+}
