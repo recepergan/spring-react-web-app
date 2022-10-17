8a7efae950587cb231efbbf9717c31e982e10dc1
recepergan <68998200+recepergan@users.noreply.github.com>
2022-10-17 16:19:37 +0300

    manual-validation

--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,11 +1,16 @@
 package com.hoaxify.ws.user;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.GenericResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -13,11 +18,21 @@ public class UserController {
 	@Autowired
 	UserService userService;
 
+
 	@PostMapping("/api/1.0/users")
 	//@ResponseStatus(HttpStatus.CREATED)
-	public GenericResponse createUser(@RequestBody User user) {
+	public ResponseEntity<?> createUser(@RequestBody User user) {
+		String username = user.getUsername();
+		if(username == null || username.isEmpty()) {
+			ApiError error = new ApiError(400,"Validation Error,",
+					"/api/1.0/users");
+			Map<String,String> validationErrors = new HashMap<>();
+			validationErrors.put("username","Username cannot be null");
+			error.setValidationErrors(validationErrors);
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		}
 		userService.save(user);
-		return new GenericResponse("user created");
+		return ResponseEntity.ok(new GenericResponse("user created"));
 
 	}
 
