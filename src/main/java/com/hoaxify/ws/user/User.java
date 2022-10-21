3b8af773c39fcb1d3330a023b4e8049243e223aa
recepergan <68998200+recepergan@users.noreply.github.com>
2022-10-21 14:24:46 +0300

    remaining-validations

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -2,9 +2,12 @@ package com.hoaxify.ws.user;
 
 
 import lombok.Data;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity// @Table ile databasedeki tablo adını istediğimiz şekilde belirleyebiliriz
@@ -15,11 +18,16 @@ public class User {
 	private long id;
 
 	@NotNull
+	@Size(min = 4,max = 255)
 	private String username;
 
 	@NotNull
+	@Size(min = 4,max = 255)
 	private String displayName;
-	
+
+	@NotNull
+	@Size(min = 4,max = 255)
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
 	private String password;
 
 	public Long getId() {
